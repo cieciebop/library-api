7a7285e51445dad457cab01db194bda2c71249e4
cici <ciciziku@gmail.com>
2019-12-17 23:40:57 +0700

    menambahkan api sederhana

---
+++ books/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Book
+from django.views.generic import ListView
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = "book_list.html"
